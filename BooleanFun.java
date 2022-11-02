600e0da0a1d5cf7bf7dc9554656a3317709524ed
smithb <smithb@wlwv.k12.or.us>
2022-11-01 22:48:16 -0700

    initial commit

---
+++ BooleanFun.java
@@ -0,0 +1,18 @@
+public class BooleanFun {
+    public static void main(String[] args) {
+        int myAge = 35;
+
+        System.out.println("Current age: " + myAge);
+
+        //convert these into something more complete using if statements
+        System.out.println("Can rent a car: " + (myAge >= 25));
+        System.out.println("Gets a senior discount: "+ (myAge >= 65));
+        System.out.println("Can order from children's menu: " + (myAge <=12));
+
+        System.out.println("On your next birthday out you will be: "); //complete this code, don't do math in sout
+
+        //further modification: prompt user for input and then print what they are able to do based on their age
+        //and print their age on their next birthday
+
+    }
+}
